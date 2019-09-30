4f2973ce9adac3475e4a09657e1ccb2056472d5b
unknown <shklyarigor2015@gmail.com>
2019-10-01 01:02:33 +0300

    Updates

---
+++ precache-manifest.8ad83edb8eba29ccc529fa0721a02dac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b573bf1fa17dc66f71c64645a8b4d464",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "b10e8f74b0451ec90e4e",
+    "url": "/react_tabs-with-router/static/css/main.91660a85.chunk.css"
+  },
+  {
+    "revision": "ed190164e008513b3cb6",
+    "url": "/react_tabs-with-router/static/js/2.53677fd4.chunk.js"
+  },
+  {
+    "revision": "b10e8f74b0451ec90e4e",
+    "url": "/react_tabs-with-router/static/js/main.459ac2be.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
